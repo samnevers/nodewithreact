ab1c48826436e7206fa9612ce8bda29649df48e3
Samme <samantha.ki@presentcompany.co>
2018-09-11 14:54:48 +1000

    changed greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 // create route handler
 app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
+	res.send({ sam: 'is the best' });
 });
 
 const PORT = process.env.PORT || 5000;
