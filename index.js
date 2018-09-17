47cdd5270894b34b378a9b6f38e1e3f8c8ce22f7
Samme <samantha.ki@presentcompany.co>
2018-09-17 17:16:16 +1000

    finished auth flow

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 const app = express();
 
-// create route handler
-app.get('/', (req, res) => {
-	res.send({ sam: 'is the best' });
-});
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey]
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// we can do the following because the require statement returns something
+// the second set of parenthesis is the argument passed into the returned function
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
